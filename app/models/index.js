83c3812119963a39c3518b5ec677789b2e7ca48b
Aishawarya222 <aishwaryagaikwad.altpi@gmail.com>
2024-06-14 12:37:16 +0530

    env add

--- app/models/index.js
+++ app/models/index.js
@@ -58,7 +58,7 @@ export const models = {
 //Dictates whether tables are modified or not;
 const flag = false; // !! KEEP IT FALSE I REPEAT KEEP IT FALSE
 
-const flag2 = false; //! ARE YOU SURE?
+const flag2 = true; //! ARE YOU SURE?
 
 const syncAllTables = async (models) => {
   if (flag) {
