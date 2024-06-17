c9089be3815cea90f63c35fb8fd921c61347e558
shubhamaltpi <shubhamaltpi@gmail.com>
2024-06-17 18:52:21 +0530

    s

--- app/controllers/biller/addNewBiller.controller.js
+++ app/controllers/biller/addNewBiller.controller.js
@@ -1,18 +1,23 @@
 import { where } from "sequelize";
 import { models } from "../../models/index.js";
-const { biller,biller_category_master,country,source_of_bill,currency,biller_frequency,agent_details} = models;
+
+const { biller,biller_category_master,country,source_of_bill,currency,biller_frequency,agent_details, billing_plan_type} = models;
 
 export const addNewBiller =async (req,res)=>{
     try {
-        const {customer_type, 
+        const {
+            biller_code,
+            customer_type, 
             biller_name,
             currency_code,
+            source_of_bill_code,
             billing_plan_type_code,
+
           bill_date_of_month,
           early_payment_date,
           discount_per,
             frequency_code,
-            biller_plan_code,
+            
             agent_id
         
          } = req.body;
@@ -20,13 +25,14 @@ export const addNewBiller =async (req,res)=>{
         const billCategory=  await biller_category_master.findOne({where:{customer_type}});
         const country_id = await country.findOne({where:{currency_code:currency_code}})
         // ------- add new biller 
-        const sourceOfBill = await source_of_bill.findOne({where:{source_of_bill_code:bill_data_feed_type}});
-        const amountType = await currency.findOne({where:{currency_code:bill_amount_type}});
-        const billFrequency = await biller_frequency.findOne({where:{frequency_code:bill_frequency}});
-        const agentId = await agent_details.findOne({where:{agent_name:agent}});
+        const billPlan = await billing_plan_type.findOne({where:{billing_plan_type_code}})
+        const sourceOfBill = await source_of_bill.findOne({where:{source_of_bill_code}});
+        const amountType = await currency.findOne({where:{currency_code}});
+        const billFrequency = await biller_frequency.findOne({where:{frequency_code}});
+        const agentId = await agent_details.findOne({where:{agent_id}});
+// -------------------- create new table 
 
-
-        biller.create({
+       await  biller.create({
             biller_code,
             biller_name,
             biller_category:billCategory.customer_type,
@@ -35,7 +41,7 @@ export const addNewBiller =async (req,res)=>{
             bill_amount_id:amountType.bill_amount_id,
             bill_freq_id:billFrequency.bill_freq_id,
             agent_id:agentId.agent_id,
-             upi_trailer,
+            
             
         });
         res.status(200).json({
