46213ed93bf8bedd56fc553a6abab8dd4cd4fa4d
shashoto <shashotolaha.altpi@gmail.com>
2024-06-26 17:29:57 +0530

    change the name

--- app/controllers/biller/allBillerInfo.controller.js
+++ app/controllers/biller/allBillerInfo.controller.js
@@ -6,7 +6,7 @@ const {
   biller_category_master,
   country,
   agent_details,
-  biller_frequency,
+  bill_frequency,
   source_of_bill,
   billing_plan_type,
 } = models;
@@ -63,7 +63,7 @@ export const allBillerInfo = async (req, res) => {
     });
 
     //! finding all billerFrequency information
-    const allFrequency = await biller_frequency.findAll({ raw: true });
+    const allFrequency = await bill_frequency.findAll({ raw: true });
 
     //! finding all biller sourceOfBill information
     const allSourceOfBill = await source_of_bill.findAll({ raw: true });
--- app/models/index.js
+++ app/models/index.js
@@ -8,7 +8,7 @@ import { biller } from "./biller.model.js";
 import { biller_bills } from "./Biller_Bills.model.js";
 import { biller_category_master } from "./biller_category_master.model.js";
 import { biller_contact } from "./biller_contact.model.js";
-import { biller_frequency } from "./biller_frequency.model.js";
+import { bill_frequency } from "./bill_frequency.model.js";
 import { country } from "./country.model.js";
 import { currency } from "./currency.model.js";
 import { customer } from "./customer.model.js";
@@ -42,7 +42,7 @@ export const models = {
   biller_bills,
   biller_category_master,
   biller_contact,
-  biller_frequency,
+  bill_frequency,
   billing_plan_type,
   country,
   currency,
