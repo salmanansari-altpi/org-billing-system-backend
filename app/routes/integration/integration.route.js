9854886fd41a3606ac728d5fdef580c18a37d5be
= <=>
2024-07-01 13:59:18 +0530

    integration.route

--- app/routes/integration/integration.route.js
+++ app/routes/integration/integration.route.js
@@ -1,19 +1,20 @@
 import express from "express";
 const router = express.Router();
-import { partnerBank } from "../../controllers/Integration/partnerbank.controller.js";
-import { getAllpartnerBanks } from "../../controllers/Integration/getPartnerBankData.controller.js";
+
+//switch
 import { createSwitch } from "../../controllers/Integration/createSwitch.controller.js";
 import { getAllSwitchData } from "../../controllers/Integration/getSwitchData.controller.js";
-import { readPayAuthority } from "../../controllers/Integration/readPayAuthority.controller.js";
-import { getProducts } from "../../controllers/Integration/product.controller.js";
-
 router.route("/createswitch").post(createSwitch);
 router.route("/getswitchdata").get(getAllSwitchData);
 
+//partnerBAnk
+import { partnerBank } from "../../controllers/Integration/partnerbank.controller.js";
+import { getAllpartnerBanks } from "../../controllers/Integration/getPartnerBankData.controller.js";
 router.route("/createpartnerBank").post(partnerBank);
 router.route("/getPartnerBank").get(getAllpartnerBanks);
 
-router.route("/payauthority").get(readPayAuthority);
-router.route("/product").get(getProducts);
+
+
+
 
 export default router;
