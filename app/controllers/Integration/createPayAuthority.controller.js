5650cbe9dec76cc8b8997fc52d905f79f6ecff6e
= <=>
2024-09-12 12:47:21 +0530

    integration file removed and api added Integration

---
+++ app/controllers/Integration/createPayAuthority.controller.js
@@ -0,0 +1,33 @@
+import { models } from "../../models/index.js";
+
+const { pay_authority } = models;
+
+export const createPayAuthority = async (req, res) => {
+  try {
+    const {
+      // payAuthority_id,
+      payAuthority_code,
+      // payAuthority_description,
+      payAuthority_name,
+      payAuthority_note,
+      payAuthority_status, //1 - Live | 2 - Suspended | 3 - Test |8 - Boarded|9 - Deleted
+    } = req.body;
+
+    await pay_authority.create({
+      payAuthority_code: payAuthority_code,
+      payAuthority_description: payAuthority_name,
+
+      payAuthority_note: payAuthority_note,
+      payAuthority_status: payAuthority_status,
+    });
+    res
+      .status(200)
+      .json({ success: true, msg: "Pay Authority created Successfully!" });
+    // res.status(200).json({success:true,msg:'Pay Authority created Successfully!',data:records })
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Unable to create ",
+    });
+  }
+};
